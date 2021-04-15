8b9784ed6adbf405dd8b2193677d902d0d50795c
vedk21 <vedantkakade.cp@gmail.com>
2021-04-15 16:53:18 +0530

    Modified: App name for spark updated

--- DataFrameTransformation/src/addRenameDropColumn.py
+++ DataFrameTransformation/src/addRenameDropColumn.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
   # Create SparkSession
   spark = (SparkSession
     .builder
-    .appName('ProjectionsAndFilters')
+    .appName('AddRenameDropColumn')
     .getOrCreate())
 
   # Input data file path
